ec176e64ac4423850c86d60219ce223401c1cf19
Varshithvhegde <varshithvh@gmail.com>
2023-03-01 19:26:27 +0530

    first commit

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; Sumanth Hegde</Col>
+          
         </Row>
       </Container>
     </footer>
